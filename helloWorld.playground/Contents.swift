17d9d5ad6b2d720a4d4ff414163b135b4fe7d568
Usuário Convidado <Guest@PA06MICRO16.local>
2017-01-30 20:42:35 -0200

    Adding a better way to declare tuplassss????git statusgit status

--- helloWorld.playground/Contents.swift
+++ helloWorld.playground/Contents.swift
@@ -28,9 +28,16 @@ print(sentence)
 //=======
 // Tuplas: collections
 
-var student = ("Cauê Almeida", 22, 10.0) // is gonna change soon
+//var student = ("Cauê Almeida", 22, 10.0) // is gonna change soon
+//var student : (String, Int, Double) = ("Cauê Almeida", 22, 10.0) // defining it
+//var student : (name: String, age: Int, grade: Double) = ("Cauê Almeida", 22, 10.0) // defining it better
+var student = (name: "Cauê Almeida", age: 22, grade: 10.0) // defining it so much better
 
 //student = ("Dunha", 99.9, 1) // Fortemente tipada
 student = ("Dunha", 99, 1)
 print(student.0)
+print(student.age)
+
+// Array
+
 

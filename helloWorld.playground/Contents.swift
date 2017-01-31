d267428d36bf09863a7891da1a2c8eb95a71a1ce
Usuário Convidado <Guest@PA06MICRO16.local>
2017-01-30 22:34:01 -0200

    About Optionals

--- helloWorld.playground/Contents.swift
+++ helloWorld.playground/Contents.swift
@@ -63,12 +63,76 @@ arrStr.sort()
 let arrTuple = [
     ("Cauê", 22),
     ("Johnior", 33),
+    ("Day", 21),
     ("Claire", 44),
     ("Clive", 52),
     ("Clyde", 12)
 ]
 
 // Define a strategy on how to order it, by what field. Not closure yet hehe
-// arrTuple.sort(by:{$0.0 < $1.0}) // Always earlier items need to be lower than the next one, this is our strategy
+// arrTuple.sort(by:{$0.0 < $1.0}) // Always earlier items need to be lower than the next one, this is our strategy. Functional Programming
+
+let firstLetterArray = arrStr.map({String($0.characters.first!)}) // $0 = array item. ! = same as toString()
+print(firstLetterArray)
+
+// ## Swift Optionals
+
+var test: Double // cannot do this
+var test2: Double? // means this variable is optional, so this var is an Double Optional
+
+test2 = 29
+print(test2)
+print(test2!) // Desembrulhando o optional - Modo VIDA LOKA, pq não verifica se tem valor antes de desembrulhar
+
+// Use Optional Binding instead
+if let testValue = test2 {
+    print(testValue)
+}
+
+// Doesn't make any sense do this, cause we were trying to avoid u know, right?!
+if let _ = test2 {
+    print(test2!)
+}
+
+if test2 != nil {
+    print(test2!)
+}
+
+// Embrulhar?????!!!!! en-US????? Un as well
+
+// With 'if let' you cannot access that variable anymore in another place, cause it just exists there, so, to do that:
+
+// guard let testValue = test2 else {/*TODO logic here*/return} // has to return, otherwise we cannot guarantee that testValue'll receive a value
+
+//testValue
+
+// guard let is many used in form validations
+
+class Person {
+    var name : String
+    init(name: String){
+        self.name = name
+    }
+    
+    func sayName() {
+        print("My name is", name)
+    }
+}
+
+// do not use + as concatenation
+let firstName = "Cauê"
+let lastName = "Almeida"
+let myAge = 22
+let phrase = "My name is \(firstName) \(lastName) and I'm \(myAge)"
+print(phrase)
+
+// we cannot do that w/ optional???? or is it about '+' concatenation??
+
+/* It works but not in this xcode version, which is 7.3.1
+var user = Person?
+user = Person(name: "Cauê")
+
+print(user?.sayName())
+*/
 
 
